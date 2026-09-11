1edf79fd66e2562fcbaa58600aa71542227c8fb0
rortiz1921 <rortiz1921@gmail.com>
2026-09-11 14:59:50 -0500

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,8 @@
-// Copia este archivo como firebase-config.js y pega la configuración
-// que te da Firebase Console > Project settings > Your apps > Web app.
 window.FIREBASE_CONFIG = {
-  apiKey: "TU_API_KEY",
-  authDomain: "TU_PROYECTO.firebaseapp.com",
-  projectId: "TU_PROJECT_ID",
-  storageBucket: "TU_PROYECTO.firebasestorage.app",
-  messagingSenderId: "TU_MESSAGING_SENDER_ID",
-  appId: "TU_APP_ID"
+  apiKey: "AIzaSyAm9W7dV0W9KFKWoIPaqOVZxQhe0iB47x8",
+  authDomain: "firetv-782bd.firebaseapp.com",
+  projectId: "firetv-782bd",
+  storageBucket: "firetv-782bd.firebasestorage.app",
+  messagingSenderId: "245303537211",
+  appId: "1:245303537211:web:52ce1cee008b8d2776f61e"
 };

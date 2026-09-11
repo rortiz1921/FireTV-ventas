d61cf738287842021fda8d327d94b32c497a0fc6
rortiz1921 <rortiz1921@gmail.com>
2026-09-11 15:08:14 -0500

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,4 +1,3 @@
-window.FIREBASE_CONFIG = {
 const firebaseConfig = {
   apiKey: "AIzaSyAm9WdlVOW9KEWoIPaqOVZxQhe0ir047x8",
   authDomain: "firetv-782bd.firebaseapp.com",
@@ -6,5 +5,4 @@ const firebaseConfig = {
   storageBucket: "firetv-782bd.firebasestorage.app",
   messagingSenderId: "245303537211",
   appId: "1:245303537211:web:52ce1cee008b8d2776f61e"
-};
 };

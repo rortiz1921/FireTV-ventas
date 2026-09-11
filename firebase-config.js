33cc7fb56e1414eae26dfe2262f29e0dc85669a8
rortiz1921 <rortiz1921@gmail.com>
2026-09-11 15:09:27 -0500

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,5 +1,5 @@
 window.FIREBASE_CONFIG = {
-  apiKey: "COPIA_AQUI_TU_API_KEY",
+  apiKey: "AIzaSyAm9WdlVOW9KEWoIPaqOVZxQhe0ir047x8",
   authDomain: "firetv-782bd.firebaseapp.com",
   projectId: "firetv-782bd",
   storageBucket: "firetv-782bd.firebasestorage.app",
